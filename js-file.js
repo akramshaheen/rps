fb58a1df0bd4a268d62704278910aba417adb703
akramshaheen <akram.shaheen27@gmail.com>
2026-03-07 07:55:50 +0100

    feat: make human choice case insensitive

--- js-file.js
+++ js-file.js
@@ -1,26 +1,16 @@
 //get user choice and make it case unsensitive (humanChoice)
-const humanChoice = prompt("choose rock, paper, or scissors:").toLowerCase();
+let humanChoice = prompt("choose rock, paper, or scissors:");
+humanChoice =
+  humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
 
 //get computer choice with a randomizer (computerChoice)
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 const computerChoice = getComputerChoice();
 
-// function getComputerChoice() {
-//   let randomizer = Math.random();
-
-//   if (randomizer < 0.33) {
-//     computerChoice = "rock";
-//   } else if (randomizer >= 0.33 && randomizer < 0.66) {
-//     computerChoice = "paper";
-//   } else {
-//     computerChoice = "scissors";
-//   }
-//   return computerChoice;
-// }
 //compare humanChoice with computerChoice and decide the winner
 //track scores and store them in two variables (humanScore, computerScore)
 
