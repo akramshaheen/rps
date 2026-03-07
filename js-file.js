dec4c2ec521d7b58025b3a1978d2d258bc015083
akramshaheen <akram.shaheen27@gmail.com>
2026-03-07 05:06:43 +0100

    chore: outline the logic behind the game

---
+++ js-file.js
@@ -0,0 +1,17 @@
+//get user choice and make it case unsensitive (humanChoice)
+//prompt choose rock, paper, or scissors:
+
+//get computer choice with a randomizer (computerChoice)
+
+//compare humanChoice with computerChoice and decide the winner
+//track scores and store them in two variables (humanScore, computerScore)
+
+//alert what each player chose, who won the round, and the current score all in one alert like this:
+
+//you chose ... and the computer chose ...
+//X won this round.
+//The score is => You: ... / The computer: ...
+
+//play the game for 5 rounds
+
+//compare humanScore and computerScore then alert; The winner is: ...
