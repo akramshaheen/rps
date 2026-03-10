ea596b16e43df8de9dea8a72c68ba72b9d07c08c
akramshaheen <akram.shaheen27@gmail.com>
2026-03-10 06:39:26 +0100

    fix: remove the two-click bug when the game ends chore: add colors to the game results text outcome

--- js-file.js
+++ js-file.js
@@ -1,6 +1,6 @@
-//Call the playGame function if a move was selected
 const playerSelection = document.querySelector(".player-selection");
-playerSelection.addEventListener("click", (e) => {
+
+const mainCallBack = (e) => {
   if (e.target.matches("#rock")) {
     playGame("Rock");
   }
@@ -12,12 +12,35 @@ playerSelection.addEventListener("click", (e) => {
   if (e.target.matches("#scissors")) {
     playGame("Scissors");
   }
-});
+};
 
-//Get computer choice
+playerSelection.addEventListener("click", mainCallBack);
+
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function GameOverState(isScoreFive) {
+  if (isScoreFive) {
+    versus.textContent = "GAME OVER";
+
+    if (humanScoreNum > computerScoreNum) {
+      roundResult.innerHTML = `You <span class="won">Won</span>`;
+    } else {
+      roundResult.innerHTML = `You <span class="lost">Lost</span>`;
+    }
+
+    playAgain.classList.add("active");
+
+    playerSelectionBtn.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    playerSelection.removeEventListener("click", mainCallBack);
+
+    return true;
+  }
 }
 
 const humanScore = document.querySelector(".human-score");
@@ -29,53 +52,14 @@ let computerScoreNum = 0;
 const versus = document.querySelector(".versus");
 const roundResult = document.querySelector(".round-result");
 
-// function gameReset(isTrue) {
-//
-// }
+const playAgain = document.querySelector(".play-again");
+const playerSelectionBtn = document.querySelectorAll(".play-btn");
+
+//Play game
 
 function playGame(humanChoice) {
-  const isGameOver = humanScoreNum === 5 || computerScoreNum === 5;
-
-  if (isGameOver) {
-    versus.textContent = "GAME OVER";
-
-    if (humanScoreNum > computerScoreNum) {
-      roundResult.textContent = "You won. Nice!";
-    } else {
-      roundResult.textContent = "You lost. Too bad!";
-    }
-
-    const playAgain = document.querySelector(".play-again");
-    playAgain.classList.add("active");
-
-    const playerSelectionBtn = document.querySelectorAll(".play-btn");
-    playerSelectionBtn.forEach((btn) => {
-      btn.classList.remove("active");
-    });
-
-    playAgain.addEventListener("click", () => {
-      humanScoreNum = 0;
-      computerScoreNum = 0;
-
-      versus.textContent = "You vs Computer";
-      roundResult.textContent = "First to five wins";
-
-      humanScore.textContent = `Player: ${humanScoreNum}`;
-      computerScore.textContent = `Computer: ${computerScoreNum}`;
-
-      playAgain.classList.remove("active");
-      playerSelectionBtn.forEach((btn) => {
-        btn.classList.add("active");
-      });
-    });
-
-    return;
-  }
-
-  //get the computer choice
   const computerChoice = getComputerChoice();
 
-  //compare humanChoice with computerChoice and decide the winner
   if (
     (humanChoice === "Rock" && computerChoice === "Scissors") ||
     (humanChoice === "Scissors" && computerChoice === "Paper") ||
@@ -84,19 +68,43 @@ function playGame(humanChoice) {
     humanScoreNum++;
     humanScore.textContent = `Player: ${humanScoreNum}`;
     versus.textContent = `${humanChoice} vs ${computerChoice}`;
-    roundResult.textContent = `You won this round!`;
+    roundResult.innerHTML = `You <span class="won">won</span> this round!`;
   } else if (humanChoice === computerChoice) {
     versus.textContent = `${humanChoice} vs ${computerChoice}`;
-    roundResult.textContent = `It's a tie!`;
+    roundResult.innerHTML = `It's a <span class="tie">tie</span>!`;
   } else {
     computerScoreNum++;
     computerScore.textContent = `Computer: ${computerScoreNum}`;
     versus.textContent = `${humanChoice} vs ${computerChoice}`;
-    roundResult.textContent = `You lost this round!`;
+    roundResult.innerHTML = `You <span class="lost">lost</span> this round!`;
+  }
+
+  const isScoreFive = humanScoreNum >= 5 || computerScoreNum >= 5;
+
+  const gameOver = GameOverState(isScoreFive);
+
+  const callBack = () => {
+    humanScoreNum = 0;
+    computerScoreNum = 0;
+
+    versus.textContent = "You vs Computer";
+    roundResult.textContent = "First to five wins";
+
+    humanScore.textContent = `Player: ${humanScoreNum}`;
+    computerScore.textContent = `Computer: ${computerScoreNum}`;
+
+    playAgain.classList.remove("active");
+
+    playerSelectionBtn.forEach((btn) => {
+      btn.classList.add("active");
+    });
+
+    playerSelection.addEventListener("click", mainCallBack);
+
+    playAgain.removeEventListener("click", callBack);
+  };
+
+  if (gameOver) {
+    playAgain.addEventListener("click", callBack);
   }
 }
-//
-//
-//
-//
-//add three variable: tie, won, and lost and give each one a color. place them inside the round-result
